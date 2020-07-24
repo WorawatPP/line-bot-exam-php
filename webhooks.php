6139946be42029390c9ce3a77f4632a693b8c320
WorawatPP <40506145+WorawatPP@users.noreply.github.com>
2020-07-24 14:18:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'LayFFV3SUqgsKb77q9eyEnCVcjzHxrpYpWzF0QDEltiGl16PnmmxgCupxN24bbJK7gKngC/IaccpP7hLXdvLugThtl2grZjgOJavTmIIy6b5AFhdNjcXgQgPY90TVyhdaL6A1Px/nCuSxU/f3+UCoAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
